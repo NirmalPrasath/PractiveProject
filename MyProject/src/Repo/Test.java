24a4fe676a187f2a3a486eb8bd2632b834a08be0
nirmalprasath <nirmalprasad.sep@gmail.com>
2018-05-27 21:39:11 +0530

    Test Commit

---
+++ MyProject/src/Repo/Test.java
@@ -0,0 +1,21 @@
+package Repo;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a[]={3,5,6,7,9,2,2};
+		
+		int n=a.length;
+		
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+
+	}
+
+}
